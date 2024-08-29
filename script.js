7cffe257da086922e14db5a8ae50cf6eec0fb497
Nikolaos Papas <116779095+NikolaosPapas@users.noreply.github.com>
2024-08-29 02:52:30 +0000

    added notes and updates

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-getElementById('funfactButton').addEventListener('click', function() {
-  const funFact = document.getElementById('funfact');
+
+// added "document" to ".GetElementById"
+document.getElementById('funFactButton').addEventListener('click', function() {
+console.log ("wassup")
+  let funFact = document.getElementById('funFact');
   if (funFact.style.display === 'none') {
       funFact.style.display = 'block';
   } else {
       funFact.style.display = 'none';
   }
-});
+})
